3a2458237c755bc0d90420efacd5e0a5a2ba6d7d
Xander Bruce <zbruce3@uncc.edu>
2024-01-21 11:16:12 -0500

    added in comments to the fourth problem

--- Int_Exercise_04.py
+++ Int_Exercise_04.py
@@ -2,6 +2,8 @@
 print()
 count : int = 0
 sum : int = 0
+
+#Take in input from the user and if it is not an integer throw an error
 while count <=4:
     try:
         sum += int(input("Enter Integer #" + str(count+1) + ": "))
@@ -10,4 +12,5 @@ while count <=4:
     except ValueError:
         print("Invalid Input, please enter an int")
 
+#Print the sum of the given integers
 print("\nYour sum is: " + str(sum), end='\n\n')
