2234d17544f48923e94d43f449b0d4c114cf0db3
Xander Bruce <zbruce3@uncc.edu>
2024-01-21 11:06:03 -0500

    Adding comments to the third problem

--- Int_Exercise_03.py
+++ Int_Exercise_03.py
@@ -1,18 +1,35 @@
 
-def string_to_Dict(string : str) -> dict:
+#Takes in a string and returns a dict mapping each character in the string to an integer value with how many times it occurs in the string
+def string_to_Dict(string : str) -> dict[str, int]:
+
+    #changes all letters to lower case
     given = string.lower()
+    #splits the string on spaces
     given = given.split(" ")
-    fullStr = ""
+    fullStr : str = ""
+
+    #add all of the split strings together
     for a in given:
         fullStr += a
+
     count : int = 0
-    outputDict : dict = {};
+
+    outputDict : dict[str, int] = {};
+
+    #for every character in the fullstr
     for a in fullStr:
+
+        #walk through the fullstr again
         for b in fullStr:
+
+            #if a character is equal to another character in the string, then we increment the count of that character
             if a == b:
                 count += 1
+
+        #map the character to the number of times that character occurs in the string.
         outputDict[a] = count
         count = 0
+
     return outputDict;
 
 
@@ -24,8 +41,15 @@ while True:
     try:
         
         s_input : str = input("Please enter a word, quote or phrase: ")
+
+        #if the user input is just a space, throw an error
         if s_input.isspace():
             raise ValueError
+
+        #if the user input is just a number throw an error
+        if s_input.isnumeric():
+            raise ValueError
+
         break 
     except ValueError:
         print("That value is incorrect,", end=' ')
